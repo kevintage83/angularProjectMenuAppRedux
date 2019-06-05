2d6f0adb616ec3050b9dfb8eb68db1d1241a43eb
Kevin Burns <kevinwburns@gmail.com>
2019-06-05 12:12:34 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projectMenuAppRedux';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+  
+  title = 'Project Menu App Redux';
 }
